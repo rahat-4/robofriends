de3a42d4ea0190ced29d6020ad16c2c086ac679f
rahat-4 <mainul.rahat4@gmail.com>
2024-03-14 15:28:46 +0600

    Bug ha been fixed

--- robofriends/src/Card.js
+++ robofriends/src/Card.js
@@ -1,6 +1,6 @@
 const Card = ({ id, name, email }) => (
   <div>
-    <img src={`"https://robohash.org/${id}"`} alt={name} />
+    <img src={`https://robohash.org/${id}`} alt={name} />
     <h3>{name}</h3>
     <p>{email}</p>
   </div>
